5e0bbf34d50346f9bea7845b8e269137f9f9526d
wellington-barreto <wellingtonbrt@gmail.com>
2026-09-06 13:48:08 -0300

    Add files via upload

---
+++ supabase/v14.1-migration.sql
@@ -0,0 +1,4 @@
+-- POWER SCALE v14.1
+-- Nenhuma alteração estrutural adicional é necessária se a migration v14.0 já foi executada.
+-- A v14.1 remove a dependência do schema-cache do PostgREST para o relacionamento financial_mining -> platforms.
+select 'POWER SCALE v14.1 - sem nova migration obrigatória' as status;
